26ba1b3aa0ff4ab435d497e7d00ab3bcb7d8a502
Baadier Sydow <bsydow@gmail.com>
2018-01-28 12:14:37 +0200

    Remove boilerplate code

--- src/providers/wordpress/wordpress.ts
+++ src/providers/wordpress/wordpress.ts
@@ -1,17 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the WordpressProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class WordpressProvider {
 
   constructor(public http: HttpClient) {
-    console.log('Hello WordpressProvider Provider');
+
   }
 
 }
